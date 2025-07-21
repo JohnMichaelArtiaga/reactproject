bba3ea109d949018769f46226708c08d33e4fd40
JohnMichaelArtiaga <johnmichaeldelecruzartiaga0312@gmail.com>
2025-07-21 20:48:23 +0800

    dropprev

--- src/DropZone.jsx
+++ src/DropZone.jsx
@@ -1,24 +1,47 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 function Dropzone() {
+  const [files, setFiles] = useState([]);
+
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    console.log(acceptedFiles);
+    setFiles(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, accept:{'application/pdf':['.pdf']}, maxFiles: 5 });
+
+  const acceptedFiles = files.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
 
   return (
-    <form class = "flex items-center justify-center">
-        <div {...getRootProps({className :'p-16 mt-10 bg-gray-800 rounded-lg shadow-lg w-[800px] h-[150px] flex justify-center'})} >
-        <input {...getInputProps()} />
-        {
-            isDragActive ?(
-            <p>Drop the files here ...</p> ):(
-            <p>Drag 'n' drop some files here, or click to select files</p>
-        ) }
-        </div>
-    </form>
-  )
+<form className="flex flex-col items-center justify-center">
+  <div
+    {...getRootProps({
+      className:
+        'p-16 mt-10 bg-gray-800 rounded-lg shadow-lg w-[800px] h-[150px] flex justify-center items-center',
+    })}
+  >
+    <input {...getInputProps()} />
+    {isDragActive ? (
+      <p className="text-white">Drop the files here ...</p>
+    ) : (
+      <p className="text-white">Drag 'n' drop some files here, or click to select files</p>
+    )}
+  </div>
+
+  <aside className="mt-4  w-[800px]">
+    <h4 className="text-white  mb-2"> Files</h4>
+    <ul className="text-white list-disc list-inside">
+      {acceptedFiles}
+    </ul>
+  </aside>
+</form>
+
+  );
 }
 
 export default Dropzone;
