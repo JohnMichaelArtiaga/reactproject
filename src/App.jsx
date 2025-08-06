d6fccf88ab65f82dfb315936708526d4eb6a60fd
JohnMichaelArtiaga <johnmichaeldelecruzartiaga0312@gmail.com>
2025-08-06 18:02:04 +0800

    drobboxonly

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Header from './Header.jsx';
 import DropZone from './DropZone.jsx';
 import Charts2 from './Charts2.jsx';
-import PdfDisplay from './PdfDisplay.jsx';
 function App() {
   return ( 
     <>
-    <PdfDisplay />
+    <DropZone />
     </>
     
   )
--- src/Dropbox.jsx
+++
@@ -1,9 +0,0 @@
-
-function Dropbox() {
-  return (
-    <div className="dropbox">
-        <Dropbox className='p-16 mt-10 bg-gray-800 rounded-lg shadow-lg' />
-    </div>
-  )
-}
-export default Dropbox;
